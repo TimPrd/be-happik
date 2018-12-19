b787b41e6fe70437240226199e5e5fc6556a0a61
Timothe Pardieu <timothe.pardieu@outlook.com>
2018-12-19 01:12:42 +0100

    setup db

--- [BACK]/app/models/index.js
+++ [BACK]/app/models/index.js
@@ -15,7 +15,4 @@ const sequelize = new Sequelize(
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-db.User = require('./User')(sequelize, Sequelize);
-db.Role = require('./Role')(sequelize, Sequelize);
-
 module.exports = db;
--- [BACK]/app/routes/router.js
+++ [BACK]/app/routes/router.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var path = require("path");
 
-const models = require('../models/index');
+const models = require('../../models/');
 
 
 //HOME
@@ -35,3 +35,72 @@ router.post("/signup", async function(req,res,next) {
 
 
 module.exports = router;
+/*
+'use strict';
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            firstName: {
+                type: Sequelize.STRING
+            },
+            lastName: {
+                type: Sequelize.STRING
+            },
+            email: {
+                type: Sequelize.STRING
+            },
+            birthday: {
+                type: Sequelize.DATE
+            },
+            avatar: {
+                type: Sequelize.STRING
+            },
+            password: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            role: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'roles',
+                    key: 'id'
+                }
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Users');
+    }
+};
+
+
+
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        firstName: DataTypes.STRING,
+        lastName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        birthday: DataTypes.DATE,
+        avatar: DataTypes.STRING,
+        password: DataTypes.STRING
+    }, {});
+    User.associate = function (models) {
+        User.belongsTo('roles'); // Adds fk_company to User
+    };
+    return User;
+};
+ */
