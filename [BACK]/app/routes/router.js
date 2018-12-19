9cd32d7fa1bdd31ec9220a734ce624dde76ded7a
Timothe Pardieu <timothe.pardieu@outlook.com>
2018-12-19 02:11:03 +0100

    DB - basic models

--- [BACK]/app/routes/router.js
+++ [BACK]/app/routes/router.js
@@ -11,7 +11,7 @@ router.route("/").all(function (req, res) {
 });
 
 router.get("/users", async function(req, res, next) {
-	const users = await models.User.findAll();
+	const users = await models.User.findAll({});
     /*User.findAll({
         where:  {
             id: 2
@@ -21,7 +21,7 @@ router.get("/users", async function(req, res, next) {
         ]
     })*/
 	console.log(users[0]);
-    res.send('Fetch : ' + users[0].firstname);//users.length + ' users.')
+    res.send('Fetch : ' + users[0].firstName);//users.length + ' users.')
 })
 
 router.post("/signup", async function(req,res,next) {
--- [BACK]/models/user.js
+++ [BACK]/models/user.js
@@ -1,15 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-        firstName: DataTypes.STRING,
-        lastName: DataTypes.STRING,
-        email: DataTypes.STRING,
-        birthday: DataTypes.DATE,
-        avatar: DataTypes.STRING,
-        password: DataTypes.STRING
-    }, {});
-    User.associate = function (models) {
-        User.belongsTo(models.Roles); // Adds fk_company to User
-    };
-    return User;
+  const User = sequelize.define('User', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    birthday: DataTypes.DATE,
+    avatar: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+      //User.belongsTo(models.Role);
+  };
+  return User;
 };
