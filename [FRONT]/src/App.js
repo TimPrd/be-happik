ae4fa5288489547b6ac8bf3c0b3492a882bb3d15
Naïm <naiim.chatti@gmail.com>
2019-01-06 16:23:45 +0100

    add styled components

--- [FRONT]/src/App.js
+++ [FRONT]/src/App.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import AddUser from './components/AddUser';
 import Dashboard from './containers/Dashboard';
-import logo from './logo.svg';
+import Theme from './utils/Theme';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+const App = () => (
+  <ThemeProvider theme={Theme}>
+    <Router>
       <div className="App">
-        <AddUser/>
-        <Dashboard/>
+        <header className="app__header">
+          {/* <NavBar /> */}
+        </header>
+
+        <main>
+          <Route path="/user/create" component={AddUser} />
+          <Route path="/dashboard" component={Dashboard} />
+        </main>
       </div>
-    );
-  }
-}
+    </Router>
+  </ThemeProvider>
+);
 
 export default App;
---
+++ [FRONT]/src/utils/Theme.js
@@ -0,0 +1,19 @@
+const theme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+    rose85: '#FC5185',
+    greenc9: '#3fc1c9',
+    greyec: ' #e8e9ec',
+    grey5c: '#4d4f5c',
+  },
+  custom: {
+    title: 30,
+    subtitle: 24,
+    bigtext: 16,
+    mediumtext: 13,
+    text: 11,
+  },
+};
+
+export default theme;
