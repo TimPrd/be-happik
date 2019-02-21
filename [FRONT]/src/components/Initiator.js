281c83d63218d572ad1444bf520ff0dd2ad22c9a
Naïm <naiim.chatti@gmail.com>
2019-02-22 00:18:09 +0100

    fix merge

--- [FRONT]/src/components/Initiator.js
+++ [FRONT]/src/components/Initiator.js
@@ -1,18 +1,20 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import io from 'socket.io-client';
 import client from '../api';
-import {UserContext} from '../contexts';
+import { UserContext } from '../contexts';
 
 import Loading from './Loading';
-import io from "socket.io-client"
 
 class Initiator extends React.Component {
-    state = {
-        loading: true,
-        user: null,
-    };
+  state = {
+    loading: true,
+    user: null,
+  };
 
+  fetchMe = async () => {
+    const loggedUser = JSON.parse(localStorage.getItem('user'));
 
     try {
       // const response = await client.get('/me');
@@ -23,7 +25,16 @@ class Initiator extends React.Component {
         },
       });
 
-      console.log(me);
+      const socket = await io.connect('http://localhost:4000/'); // TIP: io() with no args does auto-discovery
+      await socket.on('welcome', () => {
+        console.log('We are connected');
+        socket.emit('setUserId', loggedUser.id);
+        socket.emit('send', loggedUser.id);
+
+        socket.on('notification', msg => console.log(msg));
+      });
+      // setInterval(async function(){ await client.post('/api/survey/validate',{}); }, 3000);
+
 
       return me.data.msg !== 'You are not authorize'
         ? ({ user: me.data, loading: false })
@@ -32,47 +43,26 @@ class Initiator extends React.Component {
       return { user: null, loading: false };
     }
   };
-            var socket = await io.connect("http://localhost:4000/"); // TIP: io() with no args does auto-discovery
-            await socket.on('welcome', function (s) {
-                console.log("We are connected")
-                socket.emit('setUserId', loggedUser.id);
-                socket.emit('send', loggedUser.id);
-    
-                socket.on("notification", msg => console.log(msg))
-    
-            });
-            //setInterval(async function(){ await client.post('/api/survey/validate',{}); }, 3000);
-    
-
-            console.log(secret);
-
-            return secret.data.msg === 'You are authorized'
-                ? ({user: loggedUser, loading: false})
-                : ({user: null, loading: true});
-        } catch (err) {
-            return {user: null, loading: false};
-        }
-    };
 
     componentDidMount = async () => {
-        const state = await this.fetchMe();
+      const state = await this.fetchMe();
 
-        this.setState(state);
+      this.setState(state);
     };
 
     render() {
-        const {user, loading} = this.state;
-        const {children} = this.props;
-        console.log(user);
+      const { user, loading } = this.state;
+      const { children } = this.props;
+      console.log(user);
 
-        if (loading) return <Loading/>;
+      if (loading) return <Loading />;
 
-        return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+      return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
     }
 }
 
 Initiator.propTypes = {
-    children: PropTypes.element.isRequired,
+  children: PropTypes.element.isRequired,
 };
 
 export default Initiator;
